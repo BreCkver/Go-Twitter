5826866ad43767c579ae21a73a283e6d0faf5f63
Jaime R. V <breckver.dll@gmail.com>
2022-04-22 13:06:15 -0500

    se realiza correccion modelo de Datos

--- models/usuario.go
+++ models/usuario.go
@@ -9,14 +9,14 @@ import (
 /* Usuario modelo */
 type Usuario struct {
 	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Nombre          string             `bson:"nombre" json:"nombre"`
-	Apellidos       string             `bson:"apellidos" json:"apellidos"`
-	FechaNacimiento time.Time          `bson:"fechaNacimiento" json:"fechaNacimiento"`
+	Nombre          string             `bson:"nombre" json:"nombre,omitempty"`
+	Apellidos       string             `bson:"apellidos" json:"apellidos,omitempty"`
+	FechaNacimiento time.Time          `bson:"fechaNacimiento" json:"fechaNacimiento,omitempty"`
 	Email           string             `bson:"email" json:"email"`
-	Password        string             `bson:"password" json:"password"`
-	Avatar          string             `bson:"avatar" json:"avatar"`
-	Banner          string             `bson:"banner" json:"banner"`
-	Biografia       string             `bson:"biografia" json:"biografia"`
-	Ubicacion       string             `bson:"ubicacion" json:"ubicacion"`
-	SitioWeb        string             `bson:"sitioweb" json:"sitioweb"`
+	Password        string             `bson:"password" json:"password,omitempty"`
+	Avatar          string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner          string             `bson:"banner" json:"banner,omitempty"`
+	Biografia       string             `bson:"biografia" json:"biografia,omitempty"`
+	Ubicacion       string             `bson:"ubicacion" json:"ubicacion,omitempty"`
+	SitioWeb        string             `bson:"sitioweb" json:"sitioweb,omitempty"`
 }
