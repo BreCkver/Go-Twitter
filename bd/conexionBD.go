d19a78db2943259f0532916fc3001af420c41c29
Jaime R. V <breckver.dll@gmail.com>
2022-04-22 10:51:38 -0500

    update heroku

--- bd/conexionBD.go
+++ bd/conexionBD.go
@@ -13,6 +13,7 @@ var clientOptions = options.Client().ApplyURI("mongodb+srv://userApp2020:2PQ2LLM
 
 /* ConectarBD funcion que permite conectar a la BD */
 func ConectarBD() *mongo.Client {
+
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
@@ -23,13 +24,13 @@ func ConectarBD() *mongo.Client {
 		log.Fatal(err.Error())
 		return client
 	}
-
 	log.Println("Conexion exitosa con la BD")
 	return client
 }
 
 /*ChequeoConnection es resposable de validar dicha conexion */
 func ChequeoConnection() int {
+
 	err := MongoCN.Ping(context.TODO(), nil)
 	if err != nil {
 		return 0
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/BreCkver/Go-Twitter/middlew"
 	"github.com/BreCkver/Go-Twitter/routers"
-
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
@@ -16,13 +15,10 @@ import (
 func Manejadores() {
 	router := mux.NewRouter()
 	router.HandleFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
-
 	PORT := os.Getenv("PORT")
-
 	if PORT == "" {
 		PORT = "8080"
 	}
-
 	handler := cors.AllowAll().Handler(router)
 	log.Fatal(http.ListenAndServe(":"+PORT, handler))
 }
